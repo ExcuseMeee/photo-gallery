2374995ebe8c7ece9e289bd570f6d816859a78da
Oscar Lin <linoscar01@gmail.com>
2023-02-11 13:10:49 -0500

    add modal styling

--- components/AddPhoto.js
+++ components/AddPhoto.js
@@ -3,19 +3,18 @@ import { useModal } from "../context/ModalContext";
 import { useAuth } from "../context/AuthContext";
 import { useFirestore } from "../context/FirestoreContext";
 import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const AddPhoto = () => {
   const { closeModal } = useModal();
-  const { user, userData } = useAuth();
+  const { user } = useAuth();
   const { pullPhotoDocuments, addPhoto } = useFirestore();
 
   const [title, setTitle] = useState("");
   const [file, setFile] = useState(null);
 
   function handleChange(e) {
-    console.log("changed ", e);
     let selected = e.target.files[0];
-    console.log(selected);
     setFile(selected);
   }
 
@@ -25,7 +24,6 @@ const AddPhoto = () => {
       alert("Not signed in or no file");
       return;
     }
-    console.log("File name to be uploaded: ", file.name);
     await addPhoto(file, title, user.email);
     pullPhotoDocuments();
     closeModal();
@@ -46,21 +44,32 @@ const AddPhoto = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Title"
-          className="border w-1/2"
+          className="border w-1/2 outline-none p-3 rounded-lg bg-gray-50 hover:bg-gray-100 focus:bg-gray-100"
         />
-        <input
-          required
-          type="file"
-          onChange={handleChange}
-          accept={"image/*"}
-          className="border w-1/2"
-        />
-        <button type="submit" className="border ">
-          Upload
+        <label className="border w-1/2 rounded-lg hover:bg-gray-100 flex p-3">
+          <input
+            required
+            type="file"
+            onChange={handleChange}
+            accept={"image/*"}
+            hidden
+          />
+          {file ? <p>Selected File: {file.name}</p> : <p>File Select</p>}
+        </label>
+
+        <button
+          type="submit"
+          className="flex space-x-1 hover:cursor-pointer p-2 rounded-lg hover:bg-gray-100 hover:shadow-sm hover:text-blue-600 "
+        >
+          <CloudUploadIcon />
+          <p>Upload</p>
         </button>
       </form>
       <div className="w-full h-[10%] flex justify-center items-center">
-        <div className="flex space-x-1 hover:cursor-pointer p-2 rounded-lg hover:bg-gray-100 hover:shadow-sm hover:text-red-600" onClick={closeModal}>
+        <div
+          className="flex space-x-1 hover:cursor-pointer p-2 rounded-lg hover:bg-gray-100 hover:shadow-sm hover:text-red-600"
+          onClick={closeModal}
+        >
           <CancelRoundedIcon />
           <p>Cancel</p>
         </div>
